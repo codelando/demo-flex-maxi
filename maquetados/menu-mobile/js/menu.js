0fbee3f5513ec943c99c6a6b9e7ca69363758d11
Ezequiel Cortés <codelando@gmail.com>
2022-12-10 21:09:08 -0300

    Maquedato de menu mobile

--- maquetados/menu-mobile/js/menu.js
+++ maquetados/menu-mobile/js/menu.js
@@ -3,12 +3,26 @@ let botonAbrir = document.querySelector('.menu-abrir');
 let botonCerrar = document.querySelector('.menu-cerrar');
 
 function abrirMenu () {
-    menu.classList.add('activo');
+	menu.classList.add('activo');
 }
 
 function cerrarMenu () {
-    menu.classList.remove('activo');
+	menu.classList.remove('activo');
 }
 
 botonAbrir.addEventListener('click', abrirMenu);
 botonCerrar.addEventListener('click', cerrarMenu);
+
+// Detectamos cuando estamos en versión de escritorio
+let mediaqueryList = window.matchMedia("(min-width: 1024px)");
+
+$(document).ready(function(){
+	$('.sub-btn').click(function() {
+        // Si no estamos en versión de escritorio, activamos la función del submenú
+		if(!mediaqueryList.matches) {
+			$(this).next('.sub-menu').slideToggle();
+		} else {
+            $('.sub-menu').stop();
+        };
+	});
+});
